b3ca4e56deaf07434c621693816922c76381c85c
Moises Abreu <moises.abreu@logaroo.com.br>
2021-06-03 17:35:04 -0300

    feat: adicionar atributos, getters e setters da classe Event

--- src/Entities/Event.php
+++ src/Entities/Event.php
@@ -2,7 +2,86 @@
 
 namespace IfoodStrategy\Entities;
 
+/**
+ * Class Event
+ * @package IfoodStrategy\Entities
+ */
 class Event
 {
+    /** @var string $id */
+    private string $id;
 
+    /** @var string $code */
+    private string $code;
+
+    /** @var string $fullCode */
+    private string $fullCode;
+
+    /** @var string $orderId */
+    private string $orderId;
+
+    /** @var string $createdAt */
+    private string $createdAt;
+
+    /** @var array|null $metadata */
+    private ?array $metadata = [];
+
+    /**
+     * Event constructor.
+     * @param string $id
+     * @param string $code
+     * @param string $fullCode
+     * @param string $orderId
+     * @param string $createdAt
+     */
+    public function __construct(string $id, string $code, string $fullCode, string $orderId, string $createdAt)
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->fullCode = $fullCode;
+        $this->orderId = $orderId;
+        $this->createdAt = $createdAt;
+    }
+
+    /** @return string */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /** @return string */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /** @return string */
+    public function getFullCode(): string
+    {
+        return $this->fullCode;
+    }
+
+    /** @return string */
+    public function getOrderId(): string
+    {
+        return $this->orderId;
+    }
+
+    /** @return string */
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+
+    /** @return array|null */
+    public function getMetadata(): ?array
+    {
+        return $this->metadata;
+    }
+
+    /** @param array|null $metadata */
+    public function setMetadata(?array $metadata): void
+    {
+        $this->metadata = $metadata;
+    }
 }
